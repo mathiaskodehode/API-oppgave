9097cb177712f438d53c2d13b6cc211069c74755
Mathias <4mathias.kodehode@gmail.com>
2026-05-12 11:34:05 +0200

    add random images that change with you click on them

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
-export function Init() {
-    console.log("Hello, World!");
+import "./extensionMethods.js";
+import { CreateElement, RandomRange } from "./helperFunctions.js";
+
+export async function Init() {
+    const data = await getRandomImages(10);
+    data.forEach(e => {
+        CreateElement("img", null, { src: e, width: 300, height: 200, onclick: imageOnClick });
+    });
+}
+
+async function imageOnClick(e) {
+    console.log(e);
+    const image = await getRandomImages(1);
+    e.target.src = image;
+}
+
+async function getRandomImages(amount) {
+    const response = await fetch(`https://picsum.photos/v2/list?page=${RandomRange(1, 100)}&limit=${amount}`);
+    const data = await response.json();
+    const arr = data.map(e => e.download_url);
+    return arr;
 }
